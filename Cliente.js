374b3a3bfa8b3222ce3a58ada119d64b77c236e6
Éverson Vieira de Lima <107821716+EversonVieiradeLima@users.noreply.github.com>
2022-11-24 01:40:56 -0300

    Adicionando comentário aos códigos

--- Cliente.js
+++ Cliente.js
@@ -1,12 +1,24 @@
-//Criando uma classe para colocar os dados dos clientes de uma banco (Molde)
+/* (Molde) */
+/* The class Cliente has a constructor that takes two parameters, nome and cpf, and assigns them to the
+class properties nome and _cpf. The class also has a getter for the cpf property */
 export class Cliente {
   nome;
   _cpf;
 
+  /**
+   * It returns the value of the private variable _cpf.
+   * @returns The value of the private property _cpf.
+   */
   get cpf() {
     return this._cpf;
   }
 
+  /**
+   * The constructor function is a special function that is used to create and initialize an object
+   * created within a class.
+   * @param nome - The name of the person.
+   * @param cpf - The CPF number of the person.
+   */
   constructor(nome, cpf) {
     this.nome = nome;
     this._cpf = cpf;
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,34 +1,60 @@
+/* Importing the Cliente class from the Cliente.js file. */
 import { Cliente } from "./Cliente.js";
 
-//Criando uma classe para colocar os dados dos clientes de uma banco (Molde)
+/* (Molde) */
+/* ContaCorrente is a class that has a static property numeroDeContas, a property agencia, a property
+_cliente, a property _saldo, a setter cliente, a getter cliente, a getter saldo, a constructor, a
+method sacar, a method depositar, and a method transferir. */
 export class ContaCorrente {
+  /* Creating a static property called numeroDeContas and assigning it the value of 0. */
   static numeroDeContas = 0;
   agencia;
   _cliente;
   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
   _saldo = 0;
 
+  /**
+   * It checks if the new value is an instance of the Cliente class.
+   * @param novoValor - The value that is being passed to the setter.
+   */
   set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
       this._cliente = novoValor;
     }
   }
 
+  /**
+   * It returns the value of the private variable _cliente.
+   * @returns The value of the private property _cliente.
+   */
   get cliente() {
     return this._cliente;
   }
 
+  /**
+   * The getter function is a function that returns the value of the property.
+   * @returns The value of the _saldo property.
+   */
   get saldo() {
     return this._saldo;
   }
 
+  /**
+   * The constructor function is a special function that is called when a new object is created.
+   * @param agencia - number
+   * @param cliente - Cliente
+   */
   constructor(agencia, cliente) {
     this.agencia = agencia;
     this.cliente = cliente;
     ContaCorrente.numeroDeContas += 1;
   }
 
-  //Criando o método sacar
+  /**
+   * If the account has enough money, subtract the amount from the balance and return the amount.
+   * @param valor - the amount to be withdrawn
+   * @returns The value of the parameter valor.
+   */
   sacar(valor) {
     if (this._saldo >= valor) {
       //this => "dessa conta", da conta manipulada
@@ -37,13 +63,23 @@ export class ContaCorrente {
     }
   }
 
-  //Cricando o método depositar
+  /**
+   * If the value to be deposited is less than or equal to zero, the function ends. Otherwise, the
+   * value is added to the balance.
+   * @param valor - the value to be deposited
+   * @returns the value of the balance.
+   */
   depositar(valor) {
-    if (valor <= 0) return; //se o valor para deposito for menor ou igual a 0 a função encerra
+    if (valor <= 0) return;
     this._saldo += valor;
   }
 
-  //Criando o método transferir
+  /**
+   * The transferir function takes a value and a conta as arguments, and then it calls the sacar
+   * function on the value, and then it calls the depositar function on the conta.
+   * @param valor - the amount to be transferred
+   * @param conta - the account that will receive the money
+   */
   transferir(valor, conta) {
     const valorSacado = this.sacar(valor);
     conta.depositar(valorSacado);
